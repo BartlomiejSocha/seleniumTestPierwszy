ab7a23bd8f50267a682454ed5cb5bbdae17f0cba
BartłomiejSocha <bart.soha1@gmail.com>
2020-11-24 11:31:25 +0100

    waitForElement added

--- src/test/java/com/travelers/pages/HomePage.java
+++ src/test/java/com/travelers/pages/HomePage.java
@@ -1,12 +1,16 @@
 package com.travelers.pages;
 
+import com.travelers.helpers.SeleniumHelper;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class HomePage {
 
@@ -31,14 +35,31 @@ public class HomePage {
     @FindBy(id = "adultPlusBtn")
     private WebElement adultPlusBtn;
 
+    @FindBy(xpath = "//button[text()=' Search']")
+    private WebElement searchButton;
+
+    @FindBy(xpath = "//table[@class='bgwhite table table-striped']")
+    private  WebElement resultsTable;
+
+    @FindBy(xpath = "//div[@class='select2-result-label']")
+    private WebElement selectResult;
+
+    private SeleniumHelper helper;
+
+    private WebDriver driver;
+
     public HomePage(WebDriver driver) {
         PageFactory.initElements(driver, this);
+        this.helper = new SeleniumHelper(driver);
+        this.driver = driver;
     }
 
-    public void setCityHotel(String cityName) throws InterruptedException {
+    public void setCityHotel(String cityName) {
         searchSpan.click();
         searchCityInput.sendKeys(cityName);
-        Thread.sleep(5000);
+/*        By locationLabel = By.xpath("//div[@class='select2-result-label']");
+        helper.waitForElementToBeDisplayed(locationLabel);*/
+        helper.waitForElementToBeDisplayed(selectResult);
         searchCityInput.sendKeys(Keys.ENTER);
     }
 
@@ -50,7 +71,7 @@ public class HomePage {
 
     public void openTravellersModel() throws InterruptedException {
         travellersInput.click();
-        Thread.sleep(2000);
+        Thread.sleep(3000);
     }
 
     public void addAdult() {
@@ -59,6 +80,28 @@ public class HomePage {
 
     public void addChild() {
         childPlusBtn.click();
+    }
+
+    public void performSearch() throws InterruptedException {
+        searchButton.click();
+        Thread.sleep(2000);
+    }
+
+    public List<String> getHotelNames() {
+        List<String> hotelNames = new ArrayList<>();
+        List<WebElement> hotelNameWebElements = resultsTable.findElements(By.xpath("//h4//b"));
+        for (WebElement hotelNameElement : hotelNameWebElements) {
+            System.out.println(hotelNameElement.getText());
+            hotelNames.add(hotelNameElement.getText());
+        }
+        return hotelNames;
+    }
+
+    public List<String> getHotelPrices() {
+
+        List<WebElement> hotelPrices = resultsTable.findElements(By.xpath("//div[contains(@class,'price_tab')]//b"));
+        List<String> prices = hotelPrices.stream().map(element -> element.getText()).collect(Collectors.toList());
+        return prices;
     }
 
 }
--- src/test/java/com/travelers/tests/SearchHotelTest.java
+++ src/test/java/com/travelers/tests/SearchHotelTest.java
@@ -1,15 +1,15 @@
 package com.travelers.tests;
 
 import com.travelers.pages.HomePage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
+import java.util.List;
 
 public class SearchHotelTest extends BaseSeleniumTest {
 
     @Test
     public void searchHotelTest() throws InterruptedException {
-        driver.manage().timeouts().implicitlyWait(15L, TimeUnit.SECONDS);
         driver.get("http://www.kurs-selenium.pl/demo/");
         HomePage homePage = new HomePage(driver);
         homePage.setCityHotel("Dubai");
@@ -17,5 +17,18 @@ public class SearchHotelTest extends BaseSeleniumTest {
         homePage.openTravellersModel();
         homePage.addAdult();
         homePage.addChild();
+        homePage.performSearch();
+
+        List<String> hotelNames = homePage.getHotelNames();
+        Assert.assertEquals("Jumeirah Beach Hotel",hotelNames.get(0));
+        Assert.assertEquals("Oasis Beach Tower",hotelNames.get(1));
+        Assert.assertEquals("Rose Rayhaan Rotana",hotelNames.get(2));
+        Assert.assertEquals("Hyatt Regency Perth",hotelNames.get(3));
+
+        List<String> hotelPrices = homePage.getHotelPrices();
+        Assert.assertEquals("$22",hotelPrices.get(0));
+        Assert.assertEquals("$50",hotelPrices.get(1));
+        Assert.assertEquals("$80",hotelPrices.get(2));
+        Assert.assertEquals("$150",hotelPrices.get(3));
     }
 }
